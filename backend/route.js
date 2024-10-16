930af81a489a03665dcbdd2f2e5b4b826dd16752
Mridul <127869481+mriduljain0999@users.noreply.github.com>
2024-10-16 23:19:24 +0530

    Update route.js

--- backend/route.js
+++ backend/route.js
@@ -18,6 +18,10 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get("/",function(req,res){
+    res.json("Hello");
+})
+
 app.post("/signup",async function(req,res){
     try{
         const reqBody = z.object({
